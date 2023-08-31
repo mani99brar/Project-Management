644f8b47546617910818bca656180b207bb4492b
mani99brar <mani99brar@gmail.com>
2023-08-31 10:30:40 +0530

    Client Side Integration Mid

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const colors = require('colors');
+const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const port = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ const connectDB = require('./config/db');
 
 connectDB();
 
+app.use(cors());
 app.use('/graphql',graphqlHTTP({
     schema : schema,
     graphiql:process.env.NODE_ENV === 'development'
