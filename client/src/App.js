e74f18e8c8716053abf9863f92b4ddff688285b7
mani99brar <mani99brar@gmail.com>
2023-08-31 12:04:56 +0530

    Project Query Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import Header from "./components/Header";
 import {ApolloProvider,ApolloClient,InMemoryCache} from '@apollo/client';
 import Clients from "./components/Clients";
-
-
+import AddClientModal from "./components/AddClientModal";
+import Projects from "./components/Projects";
 const cache = new InMemoryCache({
   typePolicies:{
     Query:{
@@ -32,6 +32,8 @@ function App() {
     <ApolloProvider client={client}>
     <Header />
     <div className="container">
+      <AddClientModal />
+      <Projects />
       <Clients/>
     </div>
     </ApolloProvider>
